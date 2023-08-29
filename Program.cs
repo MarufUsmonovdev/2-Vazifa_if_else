fb062aa2cb0ed9ffb53c2e8262313f52e461df73
MarufUsmonovdev <142956091+MarufUsmonovdev@users.noreply.github.com>
2023-08-29 12:32:07 +0500

    Upload: Vazifa_2_Ternary

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 15;
+        int y = 15;
+
+        if (x > y)
+        {
+            System.Console.WriteLine("x is greater than y!");
+        }
+        else if(x < y)
+        {
+            System.Console.WriteLine("x is less than y!");
+        }
+        else if(x == y)
+        {
+            System.Console.WriteLine("x is equel to y!");
+        }
+        else
+        {
+            System.Console.WriteLine("x and y are not comparable!");
+        }
+
+    }
+}
